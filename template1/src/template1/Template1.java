18a3eca3a43bced7ef2349793438cfcd61baca34
ZAHID188 <zahidpranta5@gmail.com>
2021-05-24 18:28:47 +0600

    Hello world.
    Here, I've used the java  in template1 and javaweb in javawebprogram

---
+++ template1/src/template1/Template1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package template1;
+
+/**
+ *
+ * @author zahid
+ */
+public class Template1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
